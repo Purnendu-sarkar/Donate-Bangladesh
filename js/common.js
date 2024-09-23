30522af9750cb6d127a263caeb7f4c208dd82c12
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2024-09-23 21:01:20 +0600

    JS Part Complete.

---
+++ js/common.js
@@ -0,0 +1,37 @@
+function getInputValue(id) {
+    return parseFloat(document.getElementById(id).value);
+}
+function getBalance(id) {
+    return parseFloat(document.getElementById(id).innerText);
+}
+function calculate(inputAmount, myBalance, locationBalance, id, locationTitle) {
+    if (inputAmount < 0 || isNaN(inputAmount)) {
+        alert('Invalid Donation Amount!');
+    } 
+    else if (myBalance < inputAmount) {
+        alert("Insufficient balance! Please top up to proceed with your donation.");
+    } 
+    else {
+        my_modal.showModal();
+        document.getElementById(id).innerText = (locationBalance + inputAmount).toFixed(2);
+        document.getElementById('my-balance').innerText = (myBalance - inputAmount).toFixed(2);
+        addToHistory(inputAmount, locationTitle);
+    }
+}
+
+function addToHistory(inputAmount, locationTitle) {
+    const historySec = document.getElementById('history-sec');
+    const div = document.createElement('div');
+    const now = new Date();
+    const dateTimeString = now.toString();  
+    div.classList.add('flex', 'flex-col', 'gap-5', 'mb-5', 'lg:p-10', 'p-3', 'border', 'rounded-xl');
+    div.innerHTML = `
+        <h5 class="lg:text-xl font-extrabold text-text-01">
+            ${inputAmount} Taka donated to: ${locationTitle}
+        </h5>
+        <p class="text-text-02 bg-bg-light rounded-lg px-5 py-2"> Date : 
+            ${dateTimeString}
+        </p>
+    `;
+    historySec.prepend(div);
+}
---
+++ js/function.js
@@ -0,0 +1,40 @@
+
+const donationBtn = document.getElementById('donation-btn');
+const historyBtn = document.getElementById('history-btn');
+const donationSec = document.getElementById('donation-sec');
+const historySec = document.getElementById('history-sec');
+const noakhaliBtn = document.getElementById('noakhali-btn');
+const feniBtn = document.getElementById('feni-btn');
+const quotaBtn = document.getElementById('quota-btn');
+
+donationBtn.addEventListener('click', function() {
+    donationBtn.classList.add('bg-bg-green', 'text-text-01');
+    donationBtn.classList.remove('text-text-02');
+    historyBtn.classList.remove('bg-bg-green', 'text-text-01');
+    historyBtn.classList.add('text-text-02');
+    donationSec.classList.remove("hidden");
+    historySec.classList.add("hidden");
+});
+
+historyBtn.addEventListener('click', function() {
+    historyBtn.classList.add('bg-bg-green', 'text-text-01');
+    historyBtn.classList.remove('text-text-02');
+    donationBtn.classList.remove('bg-bg-green', 'text-text-01');
+    donationBtn.classList.add('text-text-02');
+    historySec.classList.remove("hidden");
+    donationSec.classList.add("hidden");
+});
+const handleDonation = (btnId, inputId, balanceId, titleId) => {
+    const btn = document.getElementById(btnId);
+    btn.addEventListener('click', function() {
+        const myBalance = getBalance('my-balance');
+        const inputAmount = getInputValue(inputId);
+        const locationBalance = getBalance(balanceId);
+        const locationTitle = document.getElementById(titleId).innerText;
+        calculate(inputAmount, myBalance, locationBalance, balanceId, locationTitle);
+    });
+};
+
+handleDonation('noakhali-btn', 'noakhali-input-value', 'noakhali-balance', 'noakhali-title');
+handleDonation('feni-btn', 'feni-input-value', 'feni-balance', 'feni-title');
+handleDonation('quota-btn', 'quota-input-value', 'quota-balance', 'quota-title');
